1d74dcf2452dcabdc8433802023204787a51eb72
genzo <genzo16@hotmail.com>
2017-06-15 00:02:38 -0300

    Update GUI

--- TPO-CLIPS/src/application/SampleController.java
+++ TPO-CLIPS/src/application/SampleController.java
@@ -1,5 +1,26 @@
 package application;
 
-public class SampleController {
-	
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class SampleController 
+{
+	@FXML
+	private ChoiceBox<String> cbox_a;
+	@FXML
+	private Tab tab_2;
+	@FXML
+	private TabPane tabPane;
+	@FXML
+	public void initialize() 
+	{ 
+	    cbox_a.getItems().removeAll(cbox_a.getItems());
+	    cbox_a.getItems().addAll("Opcion A", "Opcion B", "Opcion C");
+	    cbox_a.getSelectionModel().select("Opcion A");
+	}
+	@FXML
+	public void onSiguienteClicked() 
+	{ 
+	    tabPane.getSelectionModel().select(tab_2);
+	}
 }
